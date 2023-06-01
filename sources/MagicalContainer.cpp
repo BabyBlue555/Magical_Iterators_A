0e128c0a7d84920b94a3d3808b03fb9a11869c49
michal <michalruth144@gmail.com>
2023-06-01 15:45:21 +0300

    first commit

---
+++ sources/MagicalContainer.cpp
@@ -0,0 +1,14 @@
+#include "MagicalContainer.hpp"
+#include <iostream>
+
+#include <cmath>
+using namespace std;
+namespace ariel{
+    MagicalContainer::MagicalContainer(){}
+    void MagicalContainer::addElement(int){}
+    int MagicalContainer::size(){}
+    void MagicalContainer::removeElement(int){}
+
+};
+
+
---
+++ sources/MagicalContainer.hpp
@@ -0,0 +1,21 @@
+#ifndef MAGICALCONTAINER_HPP
+#define MAGICALCONTAINER_HPP_HPP
+
+#include <string>
+namespace ariel
+{
+	class MagicalContainer
+	{
+		private:
+
+        public:
+        MagicalContainer();
+        void addElement(int);
+        void removeElement(int);
+        int size();
+        
+
+
+	};
+};
+#endif
