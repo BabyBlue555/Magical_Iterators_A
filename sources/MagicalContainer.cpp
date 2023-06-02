5701d1dc62b1ccf6f8ce10401338f78e21c20ff6
michal <michalruth144@gmail.com>
2023-06-02 14:44:17 +0300

    start to write classes

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -1,13 +1,22 @@
 #include "MagicalContainer.hpp"
 #include <iostream>
 
-#include <cmath>
+// #include <cmath>
 using namespace std;
 namespace ariel{
+    
     MagicalContainer::MagicalContainer(){}
     void MagicalContainer::addElement(int){}
     int MagicalContainer::size(){}
     void MagicalContainer::removeElement(int){}
+
+    MagicalContainer::AscendingIterator::AscendingIterator( MagicalContainer& container) : container(container), current(0){}
+    MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operator=(const AscendingIterator& other){}
+    int& MagicalContainer::AscendingIterator::operator*() const{}
+    MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operator++(){}
+
+
+
 
 };
 
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -7,15 +7,49 @@ namespace ariel
 	class MagicalContainer
 	{
 		private:
+            
+
 
         public:
         MagicalContainer();
         void addElement(int);
         void removeElement(int);
-        int size();
+        virtual int size();
         
 
 
-	};
+	
+
+    public:
+        class AscendingIterator{
+            private:
+                MagicalContainer& container;
+                size_t current;
+            public:
+                AscendingIterator( MagicalContainer& container);
+                AscendingIterator& operator=(const AscendingIterator& other); // assignment
+                ~AscendingIterator()=default; // destructor
+
+                int& operator*() const; // dereference
+                AscendingIterator& operator++(); // increment
+                const AscendingIterator operator++(int);
+                bool operator==(const AscendingIterator& rhs) const; // compare
+                bool operator!=(const AscendingIterator& rhs) const; // compare
+                bool operator>(const AscendingIterator& other) const; // greater 
+                bool operator<(const AscendingIterator& other) const; // smaller
+                
+                AscendingIterator begin();
+                AscendingIterator end();
+
+
+        
+
+
+        }; // end of AscendingIterator class
+
+     
+
+
+    };
 };
 #endif
